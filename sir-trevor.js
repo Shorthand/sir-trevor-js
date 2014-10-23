c70ffe33f4092f59664ac678705d155b00d16fe7
Andrew Cobby <andrew.cobby@gmail.com>
2014-10-23 16:00:10 +1000

    Build

--- sir-trevor.js
+++ sir-trevor.js
@@ -2807,7 +2807,7 @@
   
         // Remove non-editable content before copying
         $('[contenteditable=false]', paragraphsAfterSelection).remove();
-        $('[contenteditable=false]', selectedParagraphs).remove();
+        $('[contenteditable=false]', newQuotes).remove();
   
         // Remove the quotes and paragraphs after from the current text block
         this.removeParagraphs([].concat(paragraphsAfterSelection, newQuotes));
@@ -2837,6 +2837,7 @@
         this.ensureNoConsecutiveStyledBlocks(editor);
         this.ensureLastBlockIsText(editor);
       }
+  
     });
   
     /*
