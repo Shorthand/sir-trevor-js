c2dc38256e7ad2af5a3a54627d61030ad9edab0b
Andrew Cobby <andrew.cobby@gmail.com>
2014-10-29 16:08:17 +1000

    Normalize identical methods into base class

--- src/block-transformer/text-and-quote.js
+++ src/block-transformer/text-and-quote.js
@@ -2,69 +2,6 @@
 
   var TextAndQuote = SirTrevor.BlockTransformer.extend({
 
-    _mergeTextBlocks: function(editor, firstBlock, secondBlock, blockPositionToInsert) {
-      // Remove non-editable content before copying
-      firstBlock.find('[contenteditable=false]').remove();
-      secondBlock.find('[contenteditable=false]').remove();
-      var textFromPreviousBlock = this.convertParagraphsToText(firstBlock.find('.st-text-block').children());
-      var textFromNewlyCreatedTextBlock = this.convertParagraphsToText(secondBlock.find('.st-text-block').children());
-
-      var textForNewBlock = '';
-      if (textFromPreviousBlock.length > 0 && textFromNewlyCreatedTextBlock.length > 0) {
-        textForNewBlock = textFromPreviousBlock + '\n\n' + textFromNewlyCreatedTextBlock;
-      } else if (textFromPreviousBlock.length === 0 && textFromNewlyCreatedTextBlock.length > 0) {
-        textForNewBlock = textFromNewlyCreatedTextBlock;
-      } else if (textFromPreviousBlock.length > 0 && textFromNewlyCreatedTextBlock.length === 0) {
-        textForNewBlock = textFromPreviousBlock;
-      }
-
-      this.addTextBlock(textForNewBlock, blockPositionToInsert, editor);
-      editor.removeBlock(firstBlock.attr('id'));
-      editor.removeBlock(secondBlock.attr('id'));
-    },
-
-    _mergeIfTextBlock: function(editor, blockToCheck, firstBlock, secondBlock, blockPosition) {
-      if (this.isTextBlock(blockToCheck)) {
-        this._mergeTextBlocks(editor, firstBlock, secondBlock, blockPosition);
-      }
-    },
-
-    merge: function(range, block, blockInner, editor) {
-      var blockPosition = editor.getBlockPosition(block);
-
-      // Remove non-editable content before copying
-      $(blockInner).find('[contenteditable=false]').remove();
-      // Create a text block from the contents of the existing quote block
-      this.addTextBlock(blockInner.innerHTML, blockPosition, editor);
-
-      // remove the old quote block
-      editor.removeBlock(block.id);
-
-      var totalNumberOfBlocks = editor.blocks.length;
-      if (totalNumberOfBlocks === 1) {
-        return;
-      }
-      var newlyCreatedTextBlock = this.getBlockFromPosition(editor, blockPosition);
-      var previousBlock = this.getBlockFromPosition(editor, blockPosition - 1);
-      var nextBlock = this.getBlockFromPosition(editor, blockPosition + 1);
-
-      if (totalNumberOfBlocks === (blockPosition + 1)) {
-        //merge into the block above
-        this._mergeIfTextBlock(editor, previousBlock, previousBlock, newlyCreatedTextBlock, blockPosition);
-        return;
-      }
-
-      if (blockPosition === 0) {
-        // if block below is not a quote then merge into it
-        this._mergeIfTextBlock(editor, nextBlock, newlyCreatedTextBlock, nextBlock, blockPosition);
-      } else {
-        // merge top and bottom blocks
-        this._mergeIfTextBlock(editor, nextBlock, newlyCreatedTextBlock, nextBlock, blockPosition);
-        newlyCreatedTextBlock = this.getBlockFromPosition(editor, blockPosition);
-        this._mergeIfTextBlock(editor, previousBlock, previousBlock, newlyCreatedTextBlock, blockPosition);
-      }
-    },
-
     split: function(range, block, blockInner, editor) {
       var position = editor.getBlockPosition(block);
       var paragraphsAfterSelection = this.getParagraphsAfterSelection(range, blockInner);
