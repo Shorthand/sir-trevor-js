62d0554fba67e7fecdba8f3e7790fde84638b643
David Langford <langford.d@gmail.com>
2015-01-09 11:30:19 +1000

    Fixed x.svg 404 [SH-2086] Deleted the reference to it as we don't need it.

--- sir-trevor.js
+++ sir-trevor.js
@@ -4,7 +4,7 @@
  * Released under the MIT license
  * www.opensource.org/licenses/MIT
  *
- * 2015-01-07
+ * 2015-01-09
  */
 
 (function ($, _){
@@ -1152,7 +1152,7 @@
       className: 'delete-and-merge',
   
       attributes: {
-        html: '<svg xmlns="http://www.w3.org/2000/svg" class="iconic-x injected-svg iconic iconic-sm iconic-main" width="16" height="16" viewBox="0 0 128 128" data-src="/iconic/svg/x.svg">  <g class="iconic-metadata">    <title>X</title>  </g>  <g data-width="16" data-height="16" class="iconic-x-sm iconic-container iconic-sm" transform="scale(8)">    <path stroke="#000" stroke-width="4" stroke-linecap="square" class="iconic-property-stroke" d="M3 3l10 10m0-10l-10 10" fill="none"></path>  </g></svg>'
+        html: '<svg xmlns="http://www.w3.org/2000/svg" class="iconic-x injected-svg iconic iconic-sm iconic-main" width="16" height="16" viewBox="0 0 128 128">  <g class="iconic-metadata">    <title>X</title>  </g>  <g data-width="16" data-height="16" class="iconic-x-sm iconic-container iconic-sm" transform="scale(8)">    <path stroke="#000" stroke-width="4" stroke-linecap="square" class="iconic-property-stroke" d="M3 3l10 10m0-10l-10 10" fill="none"></path>  </g></svg>'
       }
   
     });
--- src/block.delete-and-merge.js
+++ src/block.delete-and-merge.js
@@ -11,7 +11,7 @@ SirTrevor.BlockDeleteAndMerge = (function(){
     className: 'delete-and-merge',
 
     attributes: {
-      html: '<svg xmlns="http://www.w3.org/2000/svg" class="iconic-x injected-svg iconic iconic-sm iconic-main" width="16" height="16" viewBox="0 0 128 128" data-src="/iconic/svg/x.svg">  <g class="iconic-metadata">    <title>X</title>  </g>  <g data-width="16" data-height="16" class="iconic-x-sm iconic-container iconic-sm" transform="scale(8)">    <path stroke="#000" stroke-width="4" stroke-linecap="square" class="iconic-property-stroke" d="M3 3l10 10m0-10l-10 10" fill="none"></path>  </g></svg>'
+      html: '<svg xmlns="http://www.w3.org/2000/svg" class="iconic-x injected-svg iconic iconic-sm iconic-main" width="16" height="16" viewBox="0 0 128 128">  <g class="iconic-metadata">    <title>X</title>  </g>  <g data-width="16" data-height="16" class="iconic-x-sm iconic-container iconic-sm" transform="scale(8)">    <path stroke="#000" stroke-width="4" stroke-linecap="square" class="iconic-property-stroke" d="M3 3l10 10m0-10l-10 10" fill="none"></path>  </g></svg>'
     }
 
   });
